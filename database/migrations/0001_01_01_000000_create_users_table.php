d052e83f360d54ce2fdfbf4d07ee8e8507cb3527
ndourmouhammad <ndourmouhammad@gmail.com>
2024-06-06 16:55:06 +0000

    utilisation de users en fonction de mes besois en ajoutant les attributs roles et prenom

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,11 +13,13 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('prenom');
+            $table->string('nom');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('mot_de_passe');
+            // $table->rememberToken();
+            $table->enum('role', ['admin', 'user_simple'])->default('user_simple');
             $table->timestamps();
         });
 
