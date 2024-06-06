ef4d89cb0f20247fe741b5b2cea05e7454eb8a65
ndourmouhammad <ndourmouhammad@gmail.com>
2024-06-06 17:23:39 +0000

    ajout de la table Produit

--- app/Models/Produit.php
+++ app/Models/Produit.php
@@ -2,10 +2,23 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Produit extends Model
 {
+    protected $fillable = ['reference','designation','prix_unitaire','image','etat','categorie_id'];
+
     use HasFactory;
+
+    public function categorie(): BelongsTo
+    {
+        return $this->belongsTo(Categorie::class);
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
